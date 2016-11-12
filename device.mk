508a4c81c0ae6928ea89d93475aa81358a9e37d5
ljzyal <ljzyal@gmail.com>
2016-11-12 13:44:15 +0800

    kltechnduo: Update NFC
    Change-Id: Ia9e1023000cfc93f2baa3860ecf0e8e0fa27cb1a

--- device.mk
+++ device.mk
@@ -27,7 +27,11 @@ PRODUCT_PACKAGES += \
     init.target.rc
 
 # NFC
+# See https://github.com/CyanogenMod/android_external_libnfc-nci/blob/cm-14.1/halimpl/pn54x/Android.mk#L21
+# for magic values of NXP_CHIP_TYPE.
+NXP_CHIP_TYPE := 1
 PRODUCT_PACKAGES += \
+    libpn547_fw \
     nfc_nci.pn54x.default
 
 PRODUCT_COPY_FILES += \
