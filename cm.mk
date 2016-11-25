fbce1e18dc3530b67894e82900a753aa5cb28c2a
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-25 15:15:08 +0900

    kltechnduo: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8

--- cm.mk
+++ cm.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 $(call inherit-product, device/samsung/kltechnduo/full_kltechnduo.mk)
 
 PRODUCT_DEVICE := kltechnduo
